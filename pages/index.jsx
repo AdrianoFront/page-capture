fb70e7ac18deb9e98852506ae979b05293cd71a1
AdrianoFront <adrianolefou@gmail.com>
2022-10-03 22:07:41 -0300

    Criação de tema, botão e configuração

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,15 @@
 import Title from '../src/components/title'
+// eslint-disable-next-line no-unused-vars
+import Button from '../src/components/button/button'
+// eslint-disable-next-line no-unused-vars
+import Input from '../src/components/input/input'
 
 export default function HomePage() {
-  return <Title> Conteúdo </Title>
+  return (
+    <>
+      <Title> Conteúdo </Title>
+      <input type="text" placeholder="Digite sua opinião" />
+      <button onClick={onClick}/>
+    </>
+  )
 }
