9b4f20bf7effbbaed7eef3ad1ef19f82f9a4436e
AdrianoFront <adrianolefou@gmail.com>
2022-10-05 21:59:15 -0300

    Criado componente de créditos

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Title from '../src/components/title/title'
 import Button from '../src/components/button/button'
 import Input from '../src/components/input/input'
+import Credits from '../src/components/credits/credits'
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Title> Conteúdo </Title>
       <Input type="text" placeholder="Digite sua opinião" />
       <Button onClick />
+      <Credits />
     </>
   )
 }
---
+++ src/components/credits/credits.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+
+export default function Credits() {
+  const [userList, setUserList] = useState([])
+  useEffect(() => {
+    console.log('inicializado')
+  }, [])
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await request.json()
+      setUserList(data)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+        {userList.map((user, key) => {
+            <div key={(`user-${key}`)}>(user.name</div>
+        })}    
+    </div>
+  )
+}
