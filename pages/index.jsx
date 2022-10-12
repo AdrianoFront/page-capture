f189e70db2b5d79721c82f0428b0b19f1a7f7562
AdrianoFront <adrianolefou@gmail.com>
2022-10-11 21:35:40 -0300

    Atualizado componente Main no index

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,13 @@ import Title from '../src/components/title/title'
 import Button from '../src/components/button/button'
 import Input from '../src/components/input/input'
 import Credits from '../src/components/credits/credits'
+import Main from '../src/components/main/main'
 
 export default function HomePage() {
   return (
     <>
       <Title> Conteúdo </Title>
+      <Main />
       <Input type="text" placeholder="Digite sua opinião" />
       <Button onClick />
       <Credits />
--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 const theme = {
-  primary: '#00F0E0'
+  primary: '#00F0E0',
+  secundary: '#FFBBFF'
 }
 
 export default theme
