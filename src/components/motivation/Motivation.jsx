db4fbd771223cf1ca7e73d3f35f3023802710aa8
AdrianoFront <adrianolefou@gmail.com>
2022-10-14 22:08:59 -0300

    Criado componente Motivation

---
+++ src/components/motivation/Motivation.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-undef */
+import styled from 'styled-components'
+
+const StyledMotivation = styled.div`
+  margin: 10px;
+`
+
+export default function Motivation() {
+  return (
+    <Container>
+      <h2>Nossos ideiais são:</h2>
+    </Container>
+  )
+}
