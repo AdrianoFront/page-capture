37e08ce413aba6fc2c08975e414483109dbc50a9
AdrianoFront <adrianolefou@gmail.com>
2022-10-15 22:44:42 -0300

    Criado componente Purpose

---
+++ src/components/purpose/Purpose.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const StyledPurpose = styled.div`
+  margin: 5px;
+`
+
+export default function Purpose() {
+  return (
+    <Container>
+      <h2>Nossos ideiais são:</h2>
+    </Container>
+  )
+}
