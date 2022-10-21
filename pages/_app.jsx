52e361a52c3b0c06132290f8195a90db44aa2a98
AdrianoFront <adrianolefou@gmail.com>
2022-10-20 21:50:57 -0300

    Background adicionado

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,7 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         box-sizing: border-box;
         outline: none;
+        background-color: #3a3b3b;
     }
     body {
         font-family: 'Roboto', sans-serif;
