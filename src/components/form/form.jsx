e2e1e643602721fbcdd578f80a7df5608dc08984
AdrianoFront <adrianolefou@gmail.com>
2022-10-30 21:56:06 -0300

    Inclusão de campo no formulário

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
 export default function Form() {
-  return <form></form>
+  return <form>
+    <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+  </form>
 }
