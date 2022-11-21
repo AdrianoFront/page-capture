a1683b18c4d45bc4818e5db1e947a4b829f6a070
AdrianoFront <adrianolefou@gmail.com>
2022-11-20 22:33:57 -0300

    Reorganizando componentes

--- src/components/input/input.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const StyledInput = styled.input`
-  padding: 10px 20px;
-`
-
-export default function () {
-  return <StyledInput />
-}
--- src/components/title/title.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const StyledTitle = styled.h1`
-  color: ${(props) => props.theme.primary};
-`
-
-export default function Title(props) {
-  return <StyledTitle>{props.children}</StyledTitle>
-}
