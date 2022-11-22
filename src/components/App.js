6cd1548d8a096ae7304625a3206ec82eb6e02a19
AdrianoFront <adrianolefou@gmail.com>
2022-11-21 22:02:11 -0300

    Ajustando parâmetros dos componentes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 
-const App = () => {
-  return <div>Página Início</div>
+function App() {
+  return (
+    <div>
+      <h1>Página Início</h1>
+    </div>
+  )
 }
 
 export default App
