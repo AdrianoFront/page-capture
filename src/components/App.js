df1df63b785a3a355ff6b582f0e73804598d8b0e
AdrianoFront <adrianolefou@gmail.com>
2022-11-28 21:41:12 -0300

    Criado componente card

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Container, H1, P } from '..styled'
+import { Container, H1, P, Card } from '..styled'
 
 function App() {
   return (
     <Container>
       <H1>Headline</H1>
       <H2>chamada secundária</H2>
-      <P>
-        Descrição do produto e soluções oferecidas pelo produto.
-      </P>
-    </Contai>
+      <P>Descrição do produto e soluções oferecidas pelo produto.</P>
+      <Card>{...props}</Card>
+    </Container>
   )
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <Card>
+      <h3>Característica</h3>
+      <p>{...props}</p>
+    </Card>
+  )
+}
+
+export default Card
