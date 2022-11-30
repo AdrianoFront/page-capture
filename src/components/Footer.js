28c2bce0f16ec47a1ee656dc9f63d6c2e291fbc4
AdrianoFront <adrianolefou@gmail.com>
2022-11-29 22:01:58 -0300

    Criado componente Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <Card>
+      <h3>Desenvolvido por:</h3>
+      <a>LinkdIn</a>
+      <a>Github</a>
+    </Card>
+  )
+}
+
+export default Card
